5be88ccfc95cc733e22975567ccd578d674e1874
Sanijo Đurasević <sanijo.durasevic@rimac-automobili.com>
2018-09-19 15:38:41 +0200

    Added sallary class

--- User.py
+++ User.py
@@ -27,23 +27,44 @@ class Sby():
 	"""Sallary by years"""
 	def __init__(self, sby={'Average:':2000}):
 		self.sby = sby
+		self.setSby()
+	
+	def setSby(self):
+		"""Set sby data."""	
+		prompt1 = "No. of years: \n"
+		years = input(prompt1)
+		years = int(years)
+		sby = {}
+		for counter in range (0, years):
+			prompt2 = "Year: "
+			prompt3 = "Sallary: "
+			y = input(prompt2)
+			s = input(prompt3)
+			sby[y] = s
+		self.sby = sby
 		
 	def sbyReport(self):
 		"""Print sby"""
+		sum = 0
+		i = 0
 		for key, value in self.sby.items():
 			k = key
 			v = value
-			print(str(k) + ' ' + str(v))
+			i += 1
+			sum += int(value)
+			average = sum / i
+			print("\nYear: " + str(k) + " Sallary: " + str(v))
+			print("Average sallary: " + str(average))
 		
 class advancedUser(User):
 	"""Child class of class User"""	
 	
-	def __init__(self, name, surname, age='unknown'):
+	def __init__(self, name, surname, age='unknown', sallary='unknown'):
 		"""Initialize atributes of the parent class,
 		then initialize atributes specific to advanced user"""
 		super().__init__(name, surname, age='unknown')	
 		
-		self.sallary = 'unknown'
+		self.sallary = sallary
 		self.sby = Sby()
 	
 	def setSallary(self, sallary):
@@ -70,6 +91,7 @@ user1.userData()
 user3 = User('ane', 'bell')
 user3.userData()
 
+#sby={'Average:':2000}
 user4 = advancedUser('dani', 'lovato')
 user4.setSallary(2000)
 user4.sby.sbyReport()
