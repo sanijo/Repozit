1e6822d2b539755bb50fa0a0b3c8ebf4e308e6b7
Sanijo Đurasević <sanijo.durasevic@rimac-automobili.com>
2018-09-21 13:10:49 +0200

    Changed some lines in dataManipulation2.py

--- dataManipulation.py
+++ dataManipulation.py
@@ -1,14 +1,14 @@
 #open file and read data
 txt_f = open("data.txt", "r")
 i=-1
-x=[];
+x=[]
 lines = txt_f.readlines()
 for line in lines:
 	i += 1
 #every i % n==0 line to save
-	if i % 100 == 0: 
+	if i % 2 == 0: 
 		x.append(lines[i])    
-#txt_f.close()
+txt_f.close()
 
 #save data to .txt file
 f = open("heatGeneration.txt", "w+")
--- dataManipulation2.py
+++ dataManipulation2.py
@@ -1,20 +1,20 @@
 #open file and read data
 try:
-	with open('PT1.txt') as txt_f:
-		i=-1
-		x=[];
+	with open('PT.txt') as txt_f:
 		lines = txt_f.readlines()
-		for line in lines:
-			i += 1
-#every i % n==0 line to save
-		if i % 100 == 0: 
-			x.append(lines[i]) 
 except FileNotFoundError:
-	msg = "The file doesn't exist."
-	print(msg)   
+	msg = 'File dont exist.'
+	print(msg)
+else:
+	i = -1
+	x = []
+	for line in lines:
+		i += 1
+#every i % n==0 line to save
+		if i % 10 == 0: 
+			x.append(lines[i])    
 
 #save data to .txt file
-else:
 	with open('PT_reduced.txt', 'w') as hg:
 		for item in x:
 			hg.write("%s" % item)
