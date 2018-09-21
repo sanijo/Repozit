6f97b04611daad66d59edb3e2373e2e9532fbb1c
Sanijo Đurasević <sanijo.durasevic@rimac-automobili.com>
2018-09-21 11:09:29 +0200

    Added  try-except-else block.

--- dataManipulation2.py
+++ dataManipulation2.py
@@ -1,22 +1,27 @@
 #open file and read data
-with open('data.txt') as txt_f:
-	i=-1
-	x=[];
-	lines = txt_f.readlines()
-	for line in lines:
-		i += 1
+try:
+	with open('PT1.txt') as txt_f:
+		i=-1
+		x=[];
+		lines = txt_f.readlines()
+		for line in lines:
+			i += 1
 #every i % n==0 line to save
 		if i % 100 == 0: 
-			x.append(lines[i])    
+			x.append(lines[i]) 
+except FileNotFoundError:
+	msg = "The file doesn't exist."
+	print(msg)   
 
 #save data to .txt file
-with open('heatGeneration2.txt', 'w+') as hg:
-	for item in x:
-		hg.write("%s" % item)
+else:
+	with open('PT_reduced.txt', 'w') as hg:
+		for item in x:
+			hg.write("%s" % item)
 
 #save data to .csv file
-with open('heatGeneration2.csv', 'w+') as hg:
-	for item in x:
-		hg.write("%s" % item)
+	#with open('heatGeneration2.csv', 'w') as hg:
+		#for item in x:
+			#hg.write("%s" % item)
 
 
