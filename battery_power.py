d14ccd1095454dd1bdbfa8382f9d3c466ea07269
Sanijo Đurasević <sanijo.durasevic@rimac-automobili.com>
2019-01-15 12:45:54 +0100

    changed script for heat generation battry_power-py

--- battery_power.py
+++ battery_power.py
@@ -13,10 +13,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 from scipy.interpolate import RectBivariateSpline
+import os
+import sys
 
 start_time = time.clock()
 
-log_file_list=['C02_WLTC_20181112']
+today = str(date.today().isoformat())
+dir_save = os.getcwd() + '\\' + today + '_1D_input_data'
+os.makedirs(dir_save, exist_ok=True)
+
+log_file_list=['C02_WLTC_20181112', 'C02_Grobnik_20181112']
 
 
 def pw(log):
@@ -36,17 +42,16 @@ def pw(log):
     k = [0]
     
     while i < (time.size):
-        j.append(time[i])
-        k.append(value[i])
+        j.append(round(time[i],2))
+        k.append(round(value[i], 2))
         i += 10
         
     power = pd.DataFrame({'Time' : j, 'BatteryPower' : k})  
-
-    time = power['Time']
-    value = power['BatteryPower']
-           
+    
+    os.chdir(dir_save)       
     columnsToSave = ['Time','BatteryPower']
     power[columnsToSave].to_csv("POWER_"+log+".csv", sep='\t', encoding='utf-8', index=False, header=False)
+    os.chdir('..')
     
 def speed(log):
     
@@ -65,22 +70,26 @@ def speed(log):
     k = [0]
     
     while i < (time.size):
-        j.append(time[i])
-        k.append(value[i])
+        j.append(round(time[i],2))
+        k.append(round(value[i], 2))
         i += 10
         
     speed = pd.DataFrame({'Time' : j, 'Speed' : k})  
 
-    time = speed['Time']
-    value = speed['Speed']
-           
+    os.chdir(dir_save)       
     columnsToSave = ['Time','Speed']
     speed[columnsToSave].to_csv("SPEED_"+log+".csv", sep='\t', encoding='utf-8', index=False, header=False)
+    os.chdir('..')
+
+def main():
     
-for log in log_file_list:
-    pw(log)
+    for log in log_file_list:
+        pw(log)
 
-for log in log_file_list:
-    speed(log)
+    for log in log_file_list:
+        speed(log)
 
-print("\n--- %s seconds spent overall ---" % (time.clock() - start_time)) 
+    print("\n--- %s seconds spent overall ---" % (time.clock() - start_time)) 
+
+if __name__ == "__main__":
+    main()
